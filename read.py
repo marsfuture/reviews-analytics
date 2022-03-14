062107f78b5bc859b5407fb5dc73ec108f7de9bc
Eason Wang <ks40113202@gmail.com>
2022-03-14 23:50:23 +0800

    add filtering

--- read.py
+++ read.py
@@ -14,5 +14,13 @@ for d in data:
 
 print('留言的平均長度是: ', sum_len/len(data), '資料')
 
+new = []
+for d in data:
+    if len(d) < 100:
+        new.append(d)
+print("一共有", len(new), "筆留言小於100")
+print(new[0])
+print(new[1])
+
     
 
